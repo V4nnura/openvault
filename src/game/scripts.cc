6cb84f1b40f61cdc43b1a88769095a070930fb0a
V4nnura <52894271+V4nnura@users.noreply.github.com>
2026-05-17 23:36:54 -0300

    Fix sanitizer issue in scripts.cc using @jklock fork code as basis.

--- src/game/scripts.cc
+++ src/game/scripts.cc
@@ -2107,7 +2107,7 @@ int scr_remove(int sid)
     }
 
     Script* script = &(scriptListExtent->scripts[index]);
-    if ((script->scr_flags & SCRIPT_FLAG_0x02) != 0) {
+    if ((script->scr_flags & SCRIPT_FLAG_0x01) != 0) {
         if (script->program != NULL) {
             script->program = NULL;
         }
@@ -2189,6 +2189,12 @@ int scr_remove_all()
                 Script* script = &(scriptListExtent->scripts[index]);
 
                 if ((script->scr_flags & SCRIPT_FLAG_0x10) != 0) {
+                    // NOTE: Clear dangling program pointer if script was loaded.
+                    // Fixes sanitizer issue #223: program points to freed memory after load.
+                    if ((script->scr_flags & SCRIPT_FLAG_0x01) != 0) {
+                        script->program = NULL;
+                        script->scr_flags &= ~SCRIPT_FLAG_0x01;
+                    }
                     index++;
                 } else {
                     if (index == 0 && scriptListExtent->length == 1) {
