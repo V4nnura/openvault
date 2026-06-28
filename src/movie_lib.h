6a6981878bc614f161ce9511eea5211ddf162027
V4nnura <52894271+V4nnura@users.noreply.github.com>
2026-06-28 20:26:29 -0300

    Update movie_lib.h

--- src/movie_lib.h
+++ src/movie_lib.h
@@ -7,8 +7,8 @@ namespace fallout {
 
 typedef void*(MveMallocFunc)(size_t size);
 typedef void(MveFreeFunc)(void* ptr);
-typedef bool MovieReadProc(void* handle, void* buffer, int count);
-typedef void(MovieShowFrameProc)(SDL_Surface*, int, int, int, int, int, int, int, int);
+typedef bool(MovieReadProc)(void* handle, void* buffer, int count);
+typedef void(MveShowFrameProc)(SDL_Surface*, int, int, int, int, int, int, int, int);
 typedef void(MveSetPaletteFunc)(unsigned char* palette, int start, int count);
 
 void movieLibSetMemoryProcs(MveMallocFunc* mallocProc, MveFreeFunc* freeProc);
@@ -18,7 +18,7 @@ void movieLibSetPan(int pan);
 void _MVE_sfSVGA(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9);
 void _MVE_sfCallbacks(MovieShowFrameProc* proc);
 void MveSetPalette(MveSetPaletteFunc* set_palette_func);
-void _MVE_rmCallbacks(int (*fn)());
+void MveSetShowFrame(MveShowFrameFunc* proc);
 void _sub_4F4BB(int a1);
 void _MVE_rmFrameCounts(int* frame_count_ptr, int* frame_drop_count_ptr);
 int MVE_rmPrepMovie(void* handle, int dx, int dy, unsigned char track);
